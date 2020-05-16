e051b823d46491d95968b6c3c95885e67b3f21dd
Ajay Chopra <ajaychopra156@gmail.com>
2020-05-16 12:03:59 +0530

    start2

---
+++ app/src/main/java/com/example/tab_demo2/Splash.java
@@ -0,0 +1,30 @@
+package com.example.tab_demo2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ImageView i =findViewById(R.id.imageView4);
+        i.setImageResource(R.drawable.pac);
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
